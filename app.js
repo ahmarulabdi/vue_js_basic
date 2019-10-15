20c40a51720b4078ee5cdfe979ad700e935a176d
ahmarulabdi <rndmjck@gmai.com>
2019-10-15 23:29:19 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+ new Vue({
+ 	el : '#vue-app' ,
+ 	data : {
+ 		nama : "abdi",
+ 		pekerjaan : 'rebahan'
+ 	},
+ 	methods : {
+ 		pesan : function(waktu) {
+ 			return "Selamat "+waktu + " "+this.nama
+ 		}
+ 	}
+ })
