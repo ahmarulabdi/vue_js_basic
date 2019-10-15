03edb053a9499bfec0f1a1bf85dea71289560964
ahmarulabdi <rndmjck@gmai.com>
2019-10-16 00:55:21 +0700

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
  	el : '#vue-app' ,
  	data : {
  		nama : "abdi",
- 		pekerjaan : 'rebahan'
+ 		pekerjaan : 'rebahan',
+ 		alamatWeb : 'https://kodingit.com',
+ 		alamatWebTag : "<a href='https://kodingit.com'>Koding IT</a>",
+ 		tes_input : "Standar"
  	},
  	methods : {
  		pesan : function(waktu) {
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# section 3
+# section 4
 
-data dan method
+binding data
